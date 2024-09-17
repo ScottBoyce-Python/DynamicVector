cda99b10285c81b3c0a2fb8244120358b834efba
Scott Boyce <seboyce@usgs.gov>
2024-09-17 14:21:28 -0700

    add _metadata.py

---
+++ DynamicVector/__init__.py
@@ -0,0 +1,31 @@
+from .DynamicVector import DynamicVector, __doc__
+from ._metadata import (
+    __version__,
+    __author__,
+    __email__,
+    __license__,
+    __status__,
+    __maintainer__,
+    __credits__,
+    __url__,
+    __description__,
+    __copyright__,
+)
+
+__all__ = [
+    "DynamicVector",
+]
+
+
+if __name__ == "__main__":
+    _ = __doc__
+    print(f"Version: {__version__}")
+    print(f"Author: {__author__}")
+    print(f"Email: {__email__}")
+    print(f"License: {__license__}")
+    print(f"Status: {__status__}")
+    print(f"Maintainer: {__maintainer__}")
+    print(f"Credits: {__credits__}")
+    print(f"URL: {__url__}")
+    print(f"Description: {__description__}")
+    print(f"Copyright: {__copyright__}")
