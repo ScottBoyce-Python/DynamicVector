b32f530cf6988e079e977b40c953592040ffacaf
Scott Boyce <seboyce@usgs.gov>
2024-12-18 14:44:06 -0800

    v0.2.0 release

--- DynamicVector/_metadata.py
+++ DynamicVector/_metadata.py
@@ -2,14 +2,13 @@
 Metadata associated with DynamicVector.py
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Scott E. Boyce"
 __credits__ = "Scott E. Boyce"
 __maintainer__ = "Scott E. Boyce"
 __email__ = "boyce@engineer.com"
 __license__ = "MIT"
 __status__ = "Development"  # set to "Prototype", "Development", "Production"
-__credits__ = "Contributor One, Contributor Two"
 __url__ = "https://github.com/ScottBoyce-Python/DynamicVector"
-__description__ = "Cycle datetime intervals from a start date and list of (month, day) tuples."
+__description__ = "A dynamic vector implementation using NumPy arrays, with dynamic resizing capabilities, fast appending and popping,"
 __copyright__ = "Copyright (c) 2024 Scott E. Boyce"
